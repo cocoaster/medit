485bb46e40ae5fd78b8db80432c6815f6300179c
cocoaster <montalbanocoralie@gmail.com>
2024-06-23 15:35:30 +0200

    update extensions

--- app/public/wp-content/plugins/wpforms-lite/vendor/autoload.php
+++ app/public/wp-content/plugins/wpforms-lite/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit4143005304983a787831eeebd5a20478::getLoader();
+return ComposerAutoloaderInit947963112d0f70cd0293f23a50f00325::getLoader();
